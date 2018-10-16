4babc5617478642e3c77748925eb7b4df1e1717b
Katherine Smith <katherine.lo.smith@gmail.com>
2018-10-15 17:26:16 -0700

    Complete Node Ecosystem Lab

---
+++ 01-node-ecosystem/lab/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
+
+console.log(greet('person'));
+console.log(arithmetic.add(1, 2));
+console.log(arithmetic.sub(3, 2));
